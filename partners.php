278d48654b8d74e0e9adce26266c9c00cd1e17b2
Benjamin Ding <bding.kabooshy@gmail.com>
2017-12-19 20:19:51 +1100

    Layout partners page

--- partners.php
+++ partners.php
@@ -6,12 +6,11 @@
     <main>
       <cms:embed 'nav.inc' />
       <div class="main">
-        <section class="module bg-dark-60 portfolio-page-header" data-background="assets/images/portfolio/portfolio_header_bg.jpg">
+        <section class="module bg-dark-30 about-page-header" data-background="assets/images/portfolio/portfolio_header_bg.jpg">
           <div class="container">
             <div class="row">
               <div class="col-sm-6 col-sm-offset-3">
-                <h2 class="module-title font-alt">Portfolio Single</h2>
-                <div class="module-subtitle font-serif">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</div>
+                <h2 class="module-title font-alt mb-0">Partners</h2>
               </div>
             </div>
           </div>
@@ -19,53 +18,27 @@
         <section class="module">
           <div class="container">
             <div class="row">
-              <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
-              <div class="col-sm-6 col-md-4 col-lg-4">
-                <div class="work-details">
-                  <h5 class="work-details-title font-alt">Project Details</h5>
-                  <p>The languages only differ in their grammar, their pronunciation and their most common words. Everyone realizes why a new common language would be desirable: one could refuse to pay expensive translators.</p>
-                  <ul>
-                    <li><strong>Client: </strong><span class="font-serif"><a href="#" target="_blank">SomeCompany</a></span>
-                    </li>
-                    <li><strong>Date: </strong><span class="font-serif"><a href="#" target="_blank">23 November, 2015</a></span>
-                    </li>
-                    <li><strong>Online: </strong><span class="font-serif"><a href="#" target="_blank">www.example.com</a></span>
-                    </li>
-                  </ul>
-                </div>
+              <div class="col-sm-6 col-sm-offset-3">
+                <h2 class="module-title font-alt">Gold Sponsors</h2>
               </div>
             </div>
-          </div>
-        </section>
-        <section class="module bg-dark-60 parallax-bg" data-background="assets/images/landing/why_choose_bg.png">
-          <div class="container">
-            <div class="row multi-columns-row">
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-wallet"></span></div>
-                  <h3 class="count-to font-alt" data-countto="6543"></h3>
-                  <h5 class="count-title font-serif">Dollars raised for charity</h5>
+            <div class="row">
+              <div class ="col-sm-6">
+                <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="work-details">
+                    <h5 class="work-details-title font-alt">National Australia Bank</h5>
+                    <p>The languages only differ in their grammar, their pronunciation and their most common words. </p>
+                  </div>
                 </div>
               </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-wine"></span></div>
-                  <h3 class="count-to font-alt" data-countto="8"></h3>
-                  <h5 class="count-title font-serif">Cups of wine consumed</h5>
-                </div>
-              </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-camera"></span></div>
-                  <h3 class="count-to font-alt" data-countto="184"></h3>
-                  <h5 class="count-title font-serif">Photographs taken</h5>
-                </div>
-              </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-map-pin"></span></div>
-                  <h3 class="count-to font-alt" data-countto="32"></h3>
-                  <h5 class="count-title font-serif">Locations covered</h5>
+              <div class ="col-sm-6">
+                <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="work-details">
+                    <h5 class="work-details-title font-alt">Harris C4i</h5>
+                    <p>The languages only differ in their grammar, their pronunciation and their most common words.</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -74,47 +47,56 @@
         <section class="module">
           <div class="container">
             <div class="row">
-              <div class="col-sm-6">
-                <h5 class="font-alt mb-20">Skills for this project</h5><br>
-                <h6 class="font-alt"><span class="icon-tools-2"></span> Development
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="60" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
-                </div>
-                <h6 class="font-alt"><span class="icon-strategy"></span> Branding
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="80" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
-                </div>
-                <h6 class="font-alt"><span class="icon-target"></span> Marketing
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="50" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
-                </div>
-                <h6 class="font-alt"><span class="icon-camera"></span> Photography
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="90" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
+              <div class="col-sm-6 col-sm-offset-3">
+                <h2 class="module-title font-alt">Silver Sponsors</h2>
+              </div>
+            </div>
+            <div class="row">
+              <div class ="col-sm-6">
+                <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="work-details">
+                    <h5 class="work-details-title font-alt">National Australia Bank</h5>
+                    <p>The languages only differ in their grammar, their pronunciation and their most common words. </p>
+                  </div>
                 </div>
               </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="alt-features-item mt-20">
-                  <div class="alt-features-icon"><span class="icon-tools-2"></span></div>
-                  <h3 class="alt-features-title font-alt">Development</h3>A wonderful serenity has taken possession of my entire soul like these sweet mornings.
-                </div>
-                <div class="alt-features-item">
-                  <div class="alt-features-icon"><span class="icon-strategy"></span></div>
-                  <h3 class="alt-features-title font-alt">Branding</h3>A wonderful serenity has taken possession of my entire soul like these sweet mornings.
+              <div class ="col-sm-6">
+                <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="work-details">
+                    <h5 class="work-details-title font-alt">Harris C4i</h5>
+                    <p>The languages only differ in their grammar, their pronunciation and their most common words.</p>
+                  </div>
                 </div>
               </div>
-              <div class="col-sm-6 col-md-3 col-lg-3">
-                <div class="alt-features-item mt-20">
-                  <div class="alt-features-icon"><span class="icon-target"></span></div>
-                  <h3 class="alt-features-title font-alt">Marketing</h3>A wonderful serenity has taken possession of my entire soul like these sweet mornings.
+            </div>
+          </div>
+        </section>
+        <section class="module">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-sm-offset-3">
+                <h2 class="module-title font-alt">Bronze Sponsors</h2>
+              </div>
+            </div>
+            <div class="row">
+              <div class ="col-sm-6">
+                <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="work-details">
+                    <h5 class="work-details-title font-alt">National Australia Bank</h5>
+                    <p>The languages only differ in their grammar, their pronunciation and their most common words. </p>
+                  </div>
                 </div>
-                <div class="alt-features-item">
-                  <div class="alt-features-icon"><span class="icon-camera"></span></div>
-                  <h3 class="alt-features-title font-alt">Photography</h3>A wonderful serenity has taken possession of my entire soul like these sweet mornings.
+              </div>
+              <div class ="col-sm-6">
+                <div class="col-sm-6 col-md-8 col-lg-8"><img src="assets/images/section-1.jpg" alt="Title of Image"/></div>
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="work-details">
+                    <h5 class="work-details-title font-alt">Harris C4i</h5>
+                    <p>The languages only differ in their grammar, their pronunciation and their most common words.</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -125,91 +107,24 @@
           <div class="container">
             <div class="row">
               <div class="col-sm-6 col-sm-offset-3">
-                <h2 class="module-title font-alt">Related Projects</h2>
+                <h2 class="module-title font-alt">Affiliations</h2>
               </div>
             </div>
-            <ul class="works-grid works-grid-gut works-grid-3 works-hover-w" id="works-grid">
-              <li class="work-item illustration webdesign"><a href="portfolio_single_featured_slider1.html">
-                  <div class="work-image"><img src="assets/images/portfolio/grid-portfolio1.jpg" alt="Portfolio Item"/></div>
-                  <div class="work-caption font-alt">
-                    <h3 class="work-title">Corporate Identity</h3>
-                    <div class="work-descr">Illustration</div>
-                  </div></a></li>
-              <li class="work-item marketing webdesign"><a href="portfolio_single_featured_video1.html">
-                  <div class="work-image"><img src="assets/images/portfolio/grid-portfolio6.jpg" alt="Portfolio Item"/></div>
-                  <div class="work-caption font-alt">
-                    <h3 class="work-title">Paper clip</h3>
-                    <div class="work-descr">Marketing</div>
-                  </div></a></li>
-              <li class="work-item illustration webdesign"><a href="portfolio_single_featured_image1.html">
-                  <div class="work-image"><img src="assets/images/portfolio/grid-portfolio8.jpg" alt="Portfolio Item"/></div>
-                  <div class="work-caption font-alt">
-                    <h3 class="work-title">Branding</h3>
-                    <div class="work-descr">Illustration</div>
-                  </div></a></li>
+            <ul class="works-grid works-grid-gut works-grid-5 works-hover-w" id="works-grid">
+              <li class="work-item illustration webdesign">
+                  <img src="assets/images/portfolio/grid-portfolio1.jpg" alt="Portfolio Item"/>
+              <li class="work-item marketing webdesign">
+                  <img src="assets/images/portfolio/grid-portfolio6.jpg" alt="Portfolio Item"/>
+              <li class="work-item illustration webdesign">
+                  <img src="assets/images/portfolio/grid-portfolio8.jpg" alt="Portfolio Item"/>
+              <li class="work-item illustration webdesign">
+                  <img src="assets/images/portfolio/grid-portfolio8.jpg" alt="Portfolio Item"/>
+              <li class="work-item illustration webdesign">
+                  <img src="assets/images/portfolio/grid-portfolio8.jpg" alt="Portfolio Item"/>
             </ul>
           </div>
         </section>
-        <div class="module-small bg-dark">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">About Titan</h5>
-                  <p>The languages only differ in their grammar, their pronunciation and their most common words.</p>
-                  <p>Phone: +1 234 567 89 10</p>Fax: +1 234 567 89 10
-                  <p>Email:<a href="#">somecompany@example.com</a></p>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Recent Comments</h5>
-                  <ul class="icon-list">
-                    <li>Maria on <a href="#">Designer Desk Essentials</a></li>
-                    <li>John on <a href="#">Realistic Business Card Mockup</a></li>
-                    <li>Andy on <a href="#">Eco bag Mockup</a></li>
-                    <li>Jack on <a href="#">Bottle Mockup</a></li>
-                    <li>Mark on <a href="#">Our trip to the Alps</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Blog Categories</h5>
-                  <ul class="icon-list">
-                    <li><a href="#">Photography - 7</a></li>
-                    <li><a href="#">Web Design - 3</a></li>
-                    <li><a href="#">Illustration - 12</a></li>
-                    <li><a href="#">Marketing - 1</a></li>
-                    <li><a href="#">Wordpress - 16</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Popular Posts</h5>
-                  <ul class="widget-posts">
-                    <li class="clearfix">
-                      <div class="widget-posts-image"><a href="#"><img src="assets/images/rp-1.jpg" alt="Post Thumbnail"/></a></div>
-                      <div class="widget-posts-body">
-                        <div class="widget-posts-title"><a href="#">Designer Desk Essentials</a></div>
-                        <div class="widget-posts-meta">23 january</div>
-                      </div>
-                    </li>
-                    <li class="clearfix">
-                      <div class="widget-posts-image"><a href="#"><img src="assets/images/rp-2.jpg" alt="Post Thumbnail"/></a></div>
-                      <div class="widget-posts-body">
-                        <div class="widget-posts-title"><a href="#">Realistic Business Card Mockup</a></div>
-                        <div class="widget-posts-meta">15 February</div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-        <hr class="divider-d">
         <cms:embed 'footer.inc' />
       </div>
       <div class="scroll-up"><a href="#totop"><i class="fa fa-angle-double-up"></i></a></div>
