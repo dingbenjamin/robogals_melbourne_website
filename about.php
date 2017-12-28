1a5895551bde2661af3ae0cad5abab91fbaa41a1
antheargh <antheargh@gmail.com>
2017-12-29 00:04:20 +1100

    about us + scoreboard updated

--- about.php
+++ about.php
@@ -42,10 +42,19 @@
             <div class="col-xs-12 col-md-6 col-md-offset-6 side-image-text">
               <div class="row">
                 <div class="col-sm-12">
-                  <h2 class="module-title font-alt align-left">Our office</h2>
-                  <div class="module-subtitle font-serif align-left">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</div>
-                  <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.</p>
-                  <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p>
+                  <h2 class="module-title font-alt align-left">About Us</h2>
+                  <div class="module-subtitle font-serif align-left">Who are we?</div>
+                  <p>Founded in 2008 at the University of Melbourne by Marita Cheng, Robogals now has over 32 chapters operating across the globe in Australia, the UK, USA and Japan. Marita went on to win 2012 Australian of the Year for her efforts in increasing diversity in engineering fields. The role of CEO has since been taken over by Ami Pasricha, an engineering student at Monash University. </p>
+                  <p>Robogals is made possible by our amazing volunteers from universities and young professionals of all genders. We come from a wide range expertise from engineering to law to commerce and are all united under the cause of gender equity in engineering and technology.</p>
+
+                  <div class="module-subtitle font-serif align-left">What do we do?</div>
+                  <p>
+                  Workshops include a brief introduction to the different types of engineering, and their effects on our daily lives. Students then take part in a series of engineering challenges designed to develop teamwork and problem solving skills, including programming the NXT robotics, building structures from everyday materials and more.
+                  </p>
+                  <p>Rural trips are an initiative that bring our workshops to regional areas and towns that would otherwise not have access to our services. Previous visits include Mornington, Bendigo, Geelong and Sale. </p>
+                  <p>Robogals also participates in expos and events such as the Engineers Australia conference, Lego Exhibition, FabLearn and many school careers nights!
+                  </p>
+
                 </div>
               </div>
             </div>
@@ -55,37 +64,37 @@
           <div class="container">
             <div class="row">
               <div class="col-sm-6 col-sm-offset-3">
-                <h2 class="module-title font-alt">Scoreboard</h2>
+                <h2 class="module-title font-alt">2017 in numbers</h2>
                 <div class="module-subtitle font-serif"></div>
               </div>
             </div>
             <div class="row multi-columns-row">
               <div class="col-sm-6 col-md-3 col-lg-3">
                 <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-wallet"></span></div>
+                  <div class="count-icon"><span class="icon-profile-female"></span></div>
                   <h3 class="count-to font-alt" data-countto="6543"></h3>
-                  <h5 class="count-title font-serif">Dollars raised for charity</h5>
+                  <h5 class="count-title font-serif">Girls taught</h5>
                 </div>
               </div>
               <div class="col-sm-6 col-md-3 col-lg-3">
                 <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-wine"></span></div>
+                  <div class="count-icon"><span class="icon-book-open"></span></div>
                   <h3 class="count-to font-alt" data-countto="8"></h3>
-                  <h5 class="count-title font-serif">Cups of wine consumed</h5>
+                  <h5 class="count-title font-serif">Workshops held</h5>
                 </div>
               </div>
               <div class="col-sm-6 col-md-3 col-lg-3">
                 <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-camera"></span></div>
+                  <div class="count-icon"><span class="icon-hourglass"></span></div>
                   <h3 class="count-to font-alt" data-countto="184"></h3>
-                  <h5 class="count-title font-serif">Photographs taken</h5>
+                  <h5 class="count-title font-serif">Hours volunteered</h5>
                 </div>
               </div>
               <div class="col-sm-6 col-md-3 col-lg-3">
                 <div class="count-item mb-sm-40">
-                  <div class="count-icon"><span class="icon-map-pin"></span></div>
+                  <div class="count-icon"><span class="icon-lightbulb"></span></div>
                   <h3 class="count-to font-alt" data-countto="32"></h3>
-                  <h5 class="count-title font-serif">Locations covered</h5>
+                  <h5 class="count-title font-serif">New volunteers trained</h5>
                 </div>
               </div>
             </div>
@@ -97,10 +106,12 @@
             <div class="col-xs-12 col-md-6 side-image-text">
               <div class="row">
                 <div class="col-sm-12">
-                  <h2 class="module-title font-alt align-left">Our office</h2>
-                  <div class="module-subtitle font-serif align-left">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</div>
-                  <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.</p>
-                  <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p>
+                  <h2 class="module-title font-alt align-left"></h2>
+                  <div class="module-subtitle font-serif align-left">Why do we do it?</div>
+                  <p>There is a gender disparity in the STEM workforce as well as at higher education across the globe. In Australia, women only make up 28% of this workforce, and only 14% in engineering (AUSTRALIAN MATHEMATICAL SCIENCES INSTITUTE, 2014).
+                  </p>
+                  <p>Decreasing the gender disparity in STEM fields provides more opportunity for women to generate fair income, as well as encourages professional environments that are safer and more productive for women.
+                  Not only this, the engineering industry can tangibly benefit from an increase in gender and racial diversity because a workforce made up of varying genders and minorities creates team dynamics conducive for better problem solving produces better overall business management and reflects today’s increasingly differentiated customer base, all of which lead to improved business performance.</p>
                 </div>
               </div>
             </div>
